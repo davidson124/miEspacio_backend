d204365190123d76c827f4cefabd0dba1e17553a
Juan Carlos Jiménez Gutiérrez <jangojes.dev@gmail.com>
2025-12-11 14:35:32 -0500

    [-] User.model.js: Regla 'lowercase' del campo 'password' [$] auth.route.js: POST /login al controlador loginUser()

--- src/routes/auth.route.js
+++ src/routes/auth.route.js
@@ -1,9 +1,10 @@
 import { Router } from 'express';
 import { createUser } from '../controllers/user.controller.js';
+import { loginUser } from '../controllers/auth.controller.js';
 
 const router = Router();
 
-router.post('/login',createUser);
-router.post('/register',createUser);
+router.post('/login', loginUser);
+router.post('/register', createUser);
 
 export default router;
