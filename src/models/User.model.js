3de7c2d581828ab7eb1dbdab51af0af1f4673a88
davidson124 <daferdinat@gmail.com>
2025-12-15 18:26:15 -0500

    Modificación User.model.js

--- src/models/User.model.js
+++ src/models/User.model.js
@@ -18,6 +18,11 @@ const userSchema = new Schema({
         required: true,
         trim: true,
     },
+    lastName:{
+        type: String,
+        required:true,
+        trim:true,
+    },
     telephone: {
         type: Number,
         required: false,
